16179dc459094bf3d6385252bd9f866a22b467cb
radibit <radimir.bitsov@gmail.com>
2017-09-10 17:23:01 +0200

    add custom aliases

---
+++ aliases.zsh
@@ -0,0 +1,14 @@
+# list handling
+alias ll='ls -lahF'
+
+# ip address
+alias ip="echo Your ip is; dig +short myip.opendns.com @resolver1.opendns.com;"
+
+# MAC adress changer (free wifi hack)
+alias freewifi="sudo ifconfig en0 ether `openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//'`"
+
+# Relaod zsh config
+alias zshreload="source ~/.zshrc"
+
+# create new server on given port
+alias server='open http://localhost:5555 && python -m SimpleHTTPServer'
