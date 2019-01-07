b69d3e00c7a5705f3edd03eabc807b04361f38de
radibit <radimir.bitsov@gmail.com>
2019-01-07 16:04:14 +0100

    add brew and dev shell scripts

---
+++ brew.sh
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env sh
+
+echo ""
+echo "#############################"
+echo "# Installing various programs from brew from brew.sh"
+echo "#############################"
+echo ""
+
+echo "Installing bat"
+# *********************************
+# a better `cat`
+brew install bat
